3c227cad524cd25428901c84ac4c9ccaa733bbc2
Ousager3 <pan21@student.aau.dk>
2024-04-07 00:08:48 +0200

    Scenemanagement/MainMenu

---
+++ Assets/Peter/Code/SceneChanger.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Image canvasImage;
+    public float fadeDuration = 1.0f;
+    
+    void Start()
+    {
+        StartCoroutine(StartScene());
+    }
+
+    IEnumerator StartScene()
+    {
+        FadeToBlack();
+        yield return new WaitForSeconds(20); // Venter 5 sekunder inden fade
+        yield return new WaitForSeconds(fadeDuration);
+        // Skift scene her
+        SceneManager.LoadScene("PeterXRInteraction");
+        
+        yield return null;
+    }
+
+    void FadeToBlack()
+    {
+        StartCoroutine(FadeImage(canvasImage, fadeDuration, Color.black));
+    }
+
+    IEnumerator FadeImage(Image image, float fadeTime, Color endColor)
+    {
+        Color startColor = image.color;
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            image.color = Color.Lerp(startColor, endColor, elapsedTime / fadeTime);
+            yield return null;
+        }
+
+        image.color = endColor;
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene();
+    }
+    
+}
+
