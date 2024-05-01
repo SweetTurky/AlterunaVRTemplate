93c3a91b836ec5fca814a1995110c06e8fa0d7d6
TBGSoff <svla20@student.aau.dk>
2024-05-01 13:45:17 +0200

    New NPC's + Anim + Magnus Anim +NavMesh

---
+++ Assets/Soff/Pack/MagnusAI.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MagnusAI : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent _agent;
+    [SerializeField]
+    private Transform[] _waypoints;
+    private int _currentWaypointIndex = 0;
+
+    [SerializeField]
+    private float _walkingDuration = 30f; // Time in seconds for walking
+    private float _walkingTimer = 0f;
+
+    [SerializeField]
+    private Animator _animator;
+
+    private bool _isWalking = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        if (_agent == null)
+        {
+            Debug.LogError("Nav Mesh Agent is Null.");
+        }
+
+        SetNextWaypoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isWalking)
+        {
+            _walkingTimer += Time.deltaTime;
+
+            if (!_agent.pathPending && _agent.remainingDistance < 0.5f)
+            {
+                SetNextWaypoint();
+            }
+
+            if (_walkingTimer >= _walkingDuration)
+            {
+                _isWalking = false;
+                if (_animator != null)
+                {
+                    _animator.SetBool("IsWalking", false);
+                    _animator.SetTrigger("Talking");
+                }
+            }
+        }
+    }
+
+    private void SetNextWaypoint()
+    {
+        if (_waypoints.Length == 0)
+        {
+            Debug.LogWarning("No waypoints assigned.");
+            return;
+        }
+
+        _currentWaypointIndex = (_currentWaypointIndex + 1) % _waypoints.Length;
+        _agent.SetDestination(_waypoints[_currentWaypointIndex].position);
+    }
+}
