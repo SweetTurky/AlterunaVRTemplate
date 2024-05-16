cf8a942843f1fa3e5d9f4a9f063d22eb8840fb46
TBGSoff <svla20@student.aau.dk>
2024-05-16 12:54:03 +0200

    New

---
+++ Assets/Soff/Pub-Film/Anim/RandomBounceSpeed.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBounceSpeed : MonoBehaviour
+{
+    private float intialX;
+    private float intialZ;
+    private Animator animator;
+
+
+    void Start()
+    {
+        intialX = transform.localPosition.x;
+        intialZ = transform.localPosition.z;
+
+
+        animator = GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.speed = Random.Range(0.5f, 1.5f);
+        }
+    }
+
+    void LateUpdate()
+    {
+        Vector3 position = transform.localPosition;
+        position.x = intialX;
+        position.z = intialZ;
+        transform.localPosition = position;
+    }
+}
